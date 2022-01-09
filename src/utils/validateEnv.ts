e3c18abdfa3da2f84f2d51769bfdc0d08b8dfffe
Halit İnce <halit.ince00@gmail.com>
2022-01-10 00:20:02 +0300

    feat: create environment validation function

---
+++ src/utils/validateEnv.ts
@@ -0,0 +1,10 @@
+import { cleanEnv, str, port } from 'envalid';
+
+function validateEnv(): void {
+  cleanEnv(process.env, {
+    PORT: port({ default: 3000 }),
+    NODE_ENV: str({ choices: ['development', 'production'] }),
+  })
+}
+
+export default validateEnv;
